ff15ecb6a0e0ffe5ab39eee9377d654eb45c7c33
ningyo-sitizen <jowymone26@gmail.com>
2026-05-25 21:15:43 +0700

    email update

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("OK BACKEND");
+  res.send("OK BACKEND KIANKIANKIANAKIANAKIANA");
 });
 
 app.get("/ping", (req, res) => {
