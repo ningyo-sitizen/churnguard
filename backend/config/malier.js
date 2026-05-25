dec9d62e71c94d7fdb97e55af92c8d6f6ec4d2ea
ningyo-sitizen <jowymone26@gmail.com>
2026-05-25 20:35:29 +0700

    email update

--- backend/config/malier.js
+++ backend/config/malier.js
@@ -1,19 +1,11 @@
-  const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-  transporter.verify((err, success) => {
-    if (err) {
-      console.log("Email transporter error:", err);
-    } else {
-      console.log("Email server ready");
-    }
-  });
-
-  module.exports = transporter;
+module.exports = transporter;
