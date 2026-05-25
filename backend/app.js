e55270cb113e928934e2aced6366bf446269ab71
ningyo-sitizen <jowymone26@gmail.com>
2026-05-25 20:15:49 +0700

    fixing app.js

--- backend/app.js
+++ backend/app.js
@@ -19,14 +19,14 @@ app.use(passport.initialize());
 app.use("/csv", require("./routes/csvroutes"));
 app.use("/auth", googlelogin);
 app.use("/auth", auth_normal);
-app.use("/prediction", require("./routes/predictRoutes.js"))
-app.use("/email",require("../backend/routes/emailRoutes.js"))
-app.use("/feedback",require("../backend/routes/feedback.js"))
-app.use("/api/payment", require("../backend/routes/paymentroutes.js"));
-
-
+app.use("/prediction", require("./routes/predictRoutes.js"));
+app.use("/email", require("./routes/emailRoutes.js"));
+app.use("/feedback", require("./routes/feedback.js"));
+app.use("/api/payment", require("./routes/paymentroutes.js"));
 app.use("/test", require("./routes/testRoutes.js"));
 
-app.listen(5000, () => {
-  console.log("Server jalan di port 5000, http://localhost:5000/");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server jalan di port ${PORT}`);
 });
