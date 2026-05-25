9e048ea23c82c38d1a18a96882605e80825808e1
ningyo-sitizen <jowymone26@gmail.com>
2026-05-25 20:48:10 +0700

    email update

--- backend/app.js
+++ backend/app.js
@@ -16,13 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 
-app.use("/csv", require("./routes/csvroutes"));
 app.use("/auth", googlelogin);
 app.use("/auth", auth_normal);
-app.use("/prediction", require("./routes/predictRoutes.js"));
-app.use("/email", require("./routes/emailRoutes.js"));
-app.use("/feedback", require("./routes/feedback.js"));
-app.use("/api/payment", require("./routes/paymentroutes.js"));
+// app.use("/csv", require("./routes/csvroutes"));
+// app.use("/prediction", require("./routes/predictRoutes.js"));
+// app.use("/email", require("./routes/emailRoutes.js"));
+// app.use("/feedback", require("./routes/feedback.js"));
+// app.use("/api/payment", require("./routes/paymentroutes.js"));
 app.use("/test", require("./routes/testRoutes.js"));
 app.get("/ping", (req, res) => {
   res.json({
