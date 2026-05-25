4507274f7c68ab420a6b0cd4b38757535220bcac
ningyo-sitizen <jowymone26@gmail.com>
2026-05-25 20:59:14 +0700

    email update

--- backend/app.js
+++ backend/app.js
@@ -3,39 +3,35 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const passport = require("./config/passport.js");
-
-const googlelogin = require("./routes/googleRoutes.js");
-const auth_normal = require("./routes/authRoutes.js");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(passport.initialize());
-
-//app.use("/auth", googlelogin);
-//app.use("/auth", auth_normal);
-// app.use("/csv", require("./routes/csvroutes"));
-// app.use("/prediction", require("./routes/predictRoutes.js"));
-// app.use("/email", require("./routes/emailRoutes.js"));
-// app.use("/feedback", require("./routes/feedback.js"));
-// app.use("/api/payment", require("./routes/paymentroutes.js"));
-//app.use("/test", require("./routes/testRoutes.js"));
-//app.get("/ping", (req, res) => {
-//  res.json({
-//    status: "success",
-//    message: "pong"
-//  });
-//});
 app.get("/", (req, res) => {
-  res.status(200).send("OK BACKEND");
+  res.send("OK BACKEND");
 });
-const PORT = process.env.PORT || 5000;
+
+app.get("/ping", (req, res) => {
+  res.json({
+    status: "success",
+    message: "pong"
+  });
+});
+
+// ROUTES
+app.use("/csv", require("./routes/csvRoutes.js"));
+app.use("/auth", require("./routes/googleRoutes.js"));
+app.use("/auth", require("./routes/authRoutes.js"));
+app.use("/prediction", require("./routes/predictRoutes.js"));
+app.use("/email", require("./routes/emailRoutes.js"));
+app.use("/feedback", require("./routes/feedback.js"));
+app.use("/api/payment", require("./routes/paymentroutes.js"));
+app.use("/test", require("./routes/testRoutes.js"));
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server jalan di port ${PORT}`);
-  console.log(process.env.JWT_SECRET)
 });
