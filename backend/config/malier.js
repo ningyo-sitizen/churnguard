9abe6349766eee16a7a87da36ec306516f188834
ningyo-sitizen <jowymone26@gmail.com>
2026-05-27 00:55:56 +0700

    email update ffsssaaaaaad

--- backend/config/malier.js
+++ backend/config/malier.js
@@ -1,17 +1,25 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  family: 4,
+
   tls: {
     rejectUnauthorized: false,
   },
+
   connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 15000,
 });
 
 transporter.verify((err, success) => {
