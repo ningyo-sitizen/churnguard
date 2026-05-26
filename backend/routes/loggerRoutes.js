52778f47d7e8bcd3e9cac4aafbb51af3d77aada6
ningyo-sitizen <jowymone26@gmail.com>
2026-05-26 23:08:13 +0700

    email update ff

--- backend/routes/loggerRoutes.js
+++ backend/routes/loggerRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/checktokenuser');
 const checkrole = require("../middleware/checkrole")
-const { getLogs, createLog, deleteLog } = require('../controller/loggerControllerSA');
+const { getLogs, createLog, deleteLog } = require('../controller/loggercontrollerSA');
 
 router.get('/logging', verifyToken, checkrole('admin'), getLogs);
 router.post('/logging', verifyToken, checkrole('admin'), createLog);
